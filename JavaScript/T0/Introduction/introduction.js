0882afc7b49917cc522b147edae824d09ee8a9f0
Kay <102435276+C0D3XGH@users.noreply.github.com>
2023-06-21 21:32:53 +0200

    update

---
+++ JavaScript/T0/Introduction/introduction.js
@@ -0,0 +1,6 @@
+document.getElementById("demo5").innerHTML = 5+6;
+
+//window.alert(5+6);
+//alert(6+6);
+
+console.log("Message generated from the JavaScript file to output on the console");
--- JavaScript/T1/Variables/variables.js
+++ JavaScript/T1/Variables/variables.js
@@ -1,1 +1,1 @@
-console.log("Hello World!")
+document.getElementById("demo").innerHTML = "My Frist JavaScript";
