871a5079b577765ddf403b0427ac2d484bb28934
Kay <102435276+C0D3XGH@users.noreply.github.com>
2023-06-22 03:51:03 +0200

    update

---
+++ JavaScript/T0/Basics/basics.js
@@ -0,0 +1,12 @@
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById("demo2").innerHTML = "The value of z is " + z + "."; 
+
+function myFunction(){
+    document.getElementById("demo3").innerHTML = "Hello Wordl!";
+    document.getElementById("demo4").innerHTML = "How are you?";
+}
